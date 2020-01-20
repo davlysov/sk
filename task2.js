632f6b1d94bd93ceee2a2661067d5c1c5168db1b
Alexey <36817346+davlysov@users.noreply.github.com>
2020-01-21 00:53:12 +0300

    Решение задачи 2

---
+++ task2.js
@@ -0,0 +1,43 @@
+/*
+Задача 2
+
+Оценка сложности: 2
+Оценка трудозатрат: 4 часа
+Фактические трудозатраты: 5 часов
+ 
+Сложность O(n)
+Оценка необходимой памяти: зависит от структуры data и кол-ва isin в параметрах запроса
+
+Возможные расширения:
+- кеширование data для пар date:isin и поиск среди них
+- управление объемом
+- управление временем жизни 
+- методы очистки
+- в качестве хранилища лучше использовать localStorage или IndexedDB
+
+*/
+
+const getBondsData = ( cache = [], prevDate = '', prevIsins = [] ) => async ( {date, isins} ) => {
+
+	if ( prevDate === date && prevIsins.length === isins.length && prevIsins.filter( a => isins.indexOf(a) === -1 ) ) {		
+		return cache;
+	} else {
+		
+		const result = await http.post({
+			url: `/bonds/${date}`,
+			body: isins,
+		});
+		
+		prevDate = date;
+		prevIsins = isins;
+		
+		cache = [...result];
+		
+		return result;		
+	}
+};
+
+
+
+
+
